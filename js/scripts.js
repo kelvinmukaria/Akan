4bc1a8f67579d8bf6c2cf270e3e1fbdd5c5014b5
kelvinmukaria <mukariakelvin17@gmail.com>
2020-05-11 05:38:00 +0300

    Assign a variable calculate which is a function that will find the day of the week using a predefined formula

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ var date;
 var dayOfTheWeek;
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-
+//my variable names correspond to the ids I assigned in the form
 
 var day = function(){
     century = parseInt(document.getElementById("century").value);
@@ -15,3 +15,13 @@ var day = function(){
     date = parseInt(document.getElementById("date").value);
 }
 //here we assigned a variable day that will convert user input into integers so we can calculate values 
+
+//now we write a function to find the day of the week,the calculation shall be according to the predefined formula given in the project outline
+
+var calculate = function(){
+    day(); 
+    dayOfTheWeek = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+    return(Math.floor(dayOfTheWeek));
+    console.log(dayOfTheWeek);
+
+}
