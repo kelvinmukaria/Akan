7af721c9f923dae23b488af3355edd70a972c2ca
kelvinmukaria <mukariakelvin17@gmail.com>
2020-05-11 05:28:43 +0300

    Assign a variable day that will convert user input into integers so we can calculate values and give feedback

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,12 @@ var date;
 var dayOfTheWeek;
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+
+var day = function(){
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    date = parseInt(document.getElementById("date").value);
+}
+//here we assigned a variable day that will convert user input into integers so we can calculate values 
