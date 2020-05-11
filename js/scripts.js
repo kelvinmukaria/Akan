b7c7ee71e1b17a395f4fd44cfc27c95e76ebd2d1
kelvinmukaria <mukariakelvin17@gmail.com>
2020-05-11 05:20:23 +0300

    List all variable names items for expexted output,variable names similar to ids assigned in index.html

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// first I will list all the variables I will require for the functionality of the Akan Webpage
+var century;
+var year;
+var month;
+var date;
+var dayOfTheWeek;
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
