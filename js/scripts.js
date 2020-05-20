32f04274743af39f29a6394b75f1b17efa8404db
kelvinmukaria <mukariakelvin17@gmail.com>
2020-05-20 11:00:27 +0300

    Modify js code to achieve functionality for Akan name web application

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,36 @@
-// first I will list all the variables I will require for the functionality of the Akan Webpage
-var century = document.getElementById("century").value;
-var year = document.getElementById("year").value;
-var month = document.getElementById("month").value;
-var date = document.getElementById("date").value;
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-//my variable names correspond to the ids I assigned in the form
+var day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-var day = function(){
-    century = parseInt(document.getElementById("century").value);
-    year = parseInt(document.getElementById("year").value);
-    month = parseInt(document.getElementById("month").value);
-    date = parseInt(document.getElementById("date").value);
-}
-//here we assigned a variable day that will convert user input into integers so we can calculate values 
 
-//now we write a function to find the day of the week,the calculation shall be according to the predefined formula given in the project outline
 
 var calculate = function(){
-    day(); 
-    dayOfTheWeek = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+    var century = parseInt(document.getElementById("century").value);
+    var year = parseInt(document.getElementById("year").value);
+    var month = parseInt(document.getElementById("month").value);
+    var date =  parseInt(document.getElementById("date").value);
+    var dayOfTheWeek = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+
+    
     return(Math.floor(dayOfTheWeek));
-    console.log(dayOfTheWeek);
 }
-//the expected value should be in a range of 0-6
+
 
 var checkgender = function() {
-    calculate();
+
     var gdr = document.getElementsByName("kevz"); 
     if(gdr[0].checked === true){
         gender = "male";
     }else if(gdr[1].checked === true ){
-        gender = "female";
+        gender = "female";      
     }                         
     else {
         alert("Input gender:");
-    }
+    }}
 var result = function(){
-    dayOfTheWeek = calculate();
-    checkgender();
-    }
-    //var result() is for form validation,allows user input to be processed after the conditions are met
-// for the if/else statement and since it's long,I'll use a switch statement to execute the code depending on different cases
+    var dayOfTheWeek = calculate();
+    var gender = checkgender();
+   
   switch(gender){
         case "male":
         if(dayOfTheWeek === 0){
@@ -59,7 +48,7 @@ var result = function(){
         }else { 
             document.getElementById("result").innerHTML = "Sunday" + maleNames[6];
     }
-    //this block fetches the element in the index.html and passes it through the if/else statement for malenames
+
     break;
     case "female":
         if(dayOfTheWeek === 0){
@@ -77,10 +66,8 @@ var result = function(){
         }else{ 
             document.getElementById("result").innerHTML = "Sunday" + femaleNames[6];
         }
-        //for femaleNames variable assigned at beginning of script
+    
         break;
     }
     }
     
-
-    //break to stop exexuting code if condition is mety 
